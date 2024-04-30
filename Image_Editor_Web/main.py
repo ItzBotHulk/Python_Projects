8ef42bfeb7b08b1fae7c4098de834877b067f623
Priyanshu <bothulk1620@gmail.com>
2024-05-01 03:01:59 +0530

    New Project added

---
+++ Image_Editor_Web/main.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, flash
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = 'Image_Resizere'
+ALLOWED_EXTENSIONS = { 'png', 'webp', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def processImage():
+    pass
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return "error"
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return "error no selected file"
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template("index.html")
+    
+    return render_template("index.html")
+
+app.run(debug = True, port=5000)
